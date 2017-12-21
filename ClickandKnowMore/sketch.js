9acb51575cb1e44baf52c64a5c35128cb969e7bb
jstnstvll <32495232+jstnstvll@users.noreply.github.com>
2017-12-21 12:25:04 -0800

    Update sketch.js

--- ClickandKnowMore/sketch.js
+++ ClickandKnowMore/sketch.js
@@ -2,7 +2,10 @@
 var button;
 var mouseOver;
 var car;
-var textCount = 0
+var textoptions = {
+  background:140
+  textCount:0
+};
 //loading the text
 function preload () {
 Bree = loadFont('BreeSerif-Regular.ttf');
@@ -11,7 +14,7 @@ Bree = loadFont('BreeSerif-Regular.ttf');
 // creating the canvas, and loading th eimages i used
 function setup() {
 createCanvas(2700, 2150);
-background(140);
+background(background);
 game = loadImage("nms.jpg");
 ellipse(750, 350, 200, 200);
 fill(0, random(0,255), 0);
@@ -48,7 +51,7 @@ background(140);
 }
 //chooses the font, and advances the page
 function draw() {
-  background(140);
+  background(background);
   //image(img, 1000, 200);
   textSize(80);
   textFont(Bree);
@@ -74,7 +77,7 @@ function draw() {
 //the functionpage# defines what happens on each page
 function page2() {
   createCanvas(windowWidth, windowHeight);
-  background(140);
+  background(background);
   textSize(50);
   text("Artificial Intelligence is the creation of intelligent machines. The ultimate goal of creating these machines, is having it be able to solve any problem a human could. ", 20, 100, 1000, 400);
   text("Artificial intelligence emerged after World War II. One of the first of the early researchers of artificial intelligence was English mathematician Alan Turing, who gave a lecture on the topic in 1947.", 20, 400, 1000, 500);
@@ -85,7 +88,7 @@ function page1() {
   text("Artificial-Intelligence", 700, 350);
   fill(0, 0, 0);
   createCanvas(windowWidth, windowHeight);
-  background(140);
+  background(background);
 
 }
 function page0() {
@@ -93,11 +96,11 @@ function page0() {
   text("Artificial-Intelligence", 700, 350);
   fill(0, 0, 0);
   createCanvas(windowWidth, windowHeight);
-  background(140);
+  background(background);
 
 }
 function page3() {
-  background(140);
+  background(background);
   createCanvas(windowWidth, windowHeight);
   textSize(50);
   text("In 1950, Turing published an article, “Computing Machinery and Intelligence,” that discussed the conditions for considering a machine intelligent. He developed what has come to be called the Turing test. The Turing test is that if an A.I can convince a human that they are human it should be considered intelligent. One idea of how to create an A.I which would pass the Turing test, is to have it learn from literature like a child. The term Machine Learning refers to this. One drawback is that it is hard to simulate physical experience.",100, 100, 1000, 900);
@@ -109,16 +112,17 @@ function color() {
 }
 
 function page4() {
-  background(140);
+  background(background);
   textSize(50);
   text("One of the major limitations in defining and measuring intelligence in human and nonhuman species as well as machines is the lack of adequate measurement tools. Most human measurement tools rely on rather basic reading, comprehension, and mathematical problems. For instance, to examine working memory on an IQ test, children must repeat back a list of digits. However, working memory, as a component process of intelligence, is much more complex a process than simply repeating back a series of digits. Thus, there are many types of intelligence that are not measured adequately or that are not measured at all and therefore are unable to make a contribution to the final measure of intelligence.", 10, 100, 1000, 1200);
   image(ai, 2000, 500);
 }
 function page5() {
-  background(140);
+  background(background);
   textSize(50);
   text("My interests in artificial intelligenc are the use of artificial intelligence to generate literature, and also how it could be implemented into video games as a step beyond precedural generation which has already been implemented into a few games.", 20, 50, 900, 900);
   image(game, 10, 500);
   text("One example of this would be to use the Rita p5 library which is designed for generative literature to right a story by running sources through two competing A.I.", 20, 1000, 900, 500);
 
 }
+
